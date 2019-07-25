3f93585b13cd7d07da23465c846367fa0600d320
Damien.Lu <damien.lu@ehsn.com.tw>
2019-07-25 17:28:13 +0800

    058.Length-of-Last-Word step5 第二種寫法

--- src/058.Length-of-Last-Word/Length-of-Last-Word.js
+++ src/058.Length-of-Last-Word/Length-of-Last-Word.js
@@ -17,5 +17,21 @@ Output: 5
  * @param {string} s
  * @return {number}
  */
-const lengthOfLastWord = function(s) {};
+// const lengthOfLastWord = function(s) {
+//     let sTrim = s.trim()
+//     let sArray = sTrim.split(" ")
+//     let sLastArray = sArray[sArray.length - 1]
+//     return sLastArray.length;
+// };
+const lengthOfLastWord = function(s) {
+    let sLength = 0
+    for(let i = s.length; i > 0; i--) {
+        if (s[i-1] == " " && sLength > 0) {
+            break;
+        } else if (s[i-1] !== " ") {
+            sLength++;
+        }
+    }
+    return sLength
+};
 export default lengthOfLastWord;
