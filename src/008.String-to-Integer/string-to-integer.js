a6a04a8ba1f62fb40204a96faac9ad3f4f8e85bd
TYSON <tyson.chuang@ehsn.com.tw>
2020-06-13 17:08:20 +0800

    Case 1 Pass " Input:42, target=42"

--- src/008.String-to-Integer/string-to-integer.js
+++ src/008.String-to-Integer/string-to-integer.js
@@ -2,7 +2,12 @@
  * @param {string} str
  * @return {number}
  */
-const myAtoi = function(str) {
-
-};
-export default myAtoi;
+const myAtoi = function (str) {
+    return Array.from(str)
+        .reverse()
+        .reduce((accumulator, current, index) => {
+            const digits = Math.pow(10, index)
+            return accumulator + current * digits
+        }, 0)
+}
+export default myAtoi
