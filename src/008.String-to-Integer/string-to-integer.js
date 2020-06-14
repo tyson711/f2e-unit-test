52875c447db29c49b41113ee044f1a80de33895a
TYSON <tyson.chuang@ehsn.com.tw>
2020-06-15 01:16:11 +0800

    Accepted string-to-integet

--- src/008.String-to-Integer/string-to-integer.js
+++ src/008.String-to-Integer/string-to-integer.js
@@ -4,17 +4,23 @@
  */
 
 const myAtoi = function (str) {
-    if (isNaN(str[0]) && isNaN(str[1])) return 0
-    const INT_MAX = Math.pow(2, 31)
+    let target = str.trim()
+    if (isNaN(target[0]) && isNaN(target[1])) return 0
+    const INT_MAX = Math.pow(2, 31) - 1
     const INT_MIN = Math.pow(-2, 31)
-    const target = str.trim()
     const isMinus = target[0] === '-'
+    if (isMinus || target[0] === '+') {
+        target = target.slice(1)
+    }
     let result = 0
     let index = 0
     for (let i = target.length; i >= 0; i--) {
         const digits = Math.pow(10, index)
         const currentNumber = target[i] * digits
-        if (currentNumber) {
+        if (isNaN(currentNumber) || target[i] === ' ') {
+            result = 0
+            index = 0
+        } else {
             result += currentNumber
             index++
         }
